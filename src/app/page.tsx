6b7d5ca7a212915ff3cd21eaca5b46351db45d1b
Vinicius Rossi <viniciuslimarossi7@gmail.com>
2025-08-02 06:33:55 -0300

    wip: vite start

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Terminal } from '@/components/terminal'
-
-export default function Home() {
-  return (
-    <div className="px-4 py-8 h-svh">
-      <Terminal />
-    </div>
-  )
-}
--- src/components/sections/welcome.tsx
+++
@@ -1,7 +0,0 @@
-export function Welcome() {
-  return (
-    <span>
-      Welcome $USER. Type <b>help</b> to see available commands.
-    </span>
-  )
-}
